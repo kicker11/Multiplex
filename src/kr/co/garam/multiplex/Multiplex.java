b5574ffaeead983747879da9a899a0bbd8c6cf4d
kicker <kicker@GARAM-DELL>
2018-08-03 08:24:10 +0900

    for 문을 이용한 구구단 작성

--- src/kr/co/garam/multiplex/Multiplex.java
+++ src/kr/co/garam/multiplex/Multiplex.java
@@ -4,7 +4,17 @@ public class Multiplex {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        // 구구단을 출력하는 테스트
+		
+		int i = 2 ;
+		int j = 1 ;
+		
+		for (i = 2; i < 10; i++) {
+			for (j = 1; j < 10; j++) {
+				System.out.println(String.format("%2d %s %2d %s %3d", i, "*", j, "=", (i*j))) ;
+			}
+			System.out.println("\n");
+		}
 	}
 
 }
