d92df01860c1702fe44d52806737a55e9403383e
kicker <kicker@GARAM-DELL>
2018-08-03 16:23:06 +0900

    * 사용자 입력을 위한 Scanner Class 이용 * if 문에서의 변수에 할당된 값의 비교 ("y").equals(변수명)

--- src/kr/co/garam/multiplex/Multiplex.java
+++ src/kr/co/garam/multiplex/Multiplex.java
@@ -1,5 +1,7 @@
 package kr.co.garam.multiplex;
 
+import java.util.Scanner;
+
 public class Multiplex {
 
 	public static void main(String[] args) {
@@ -8,12 +10,25 @@ public class Multiplex {
 		
 		int i = 2 ;
 		int j = 1 ;
+		int Rst = 0;
+
+		System.out.println("구구단을 출력하시겠습니까. ? [y/n]") ;
 		
-		for (i = 2; i < 10; i++) {
-			for (j = 1; j < 10; j++) {
-				System.out.println(String.format("%2d %s %2d %s %3d", i, "*", j, "=", (i*j))) ;
+		Scanner MyInput = new Scanner(System.in) ;
+		
+		String Ans = MyInput.next() ;
+			
+		
+		if ( ("y").equals(Ans)) {
+			for (i = 2; i < 10; i++) {
+				for (j = 1; j < 10; j++) {
+					Rst = i * j ;
+					System.out.println(String.format("%2d %s %2d %s %3d", i, "*", j, "=", Rst)) ;
+				}
+				System.out.println("\n");
 			}
-			System.out.println("\n");
+		} else {
+			System.out.println("취소되었습니다.") ;
 		}
 	}
 
