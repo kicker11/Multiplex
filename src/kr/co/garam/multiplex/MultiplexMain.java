490fe2cef9da33d55fe5692b11469b8b5ac93669
kicker <kicker@GARAM-DELL>
2019-01-16 16:41:30 +0900

    UPDATE

--- src/kr/co/garam/multiplex/MultiplexMain.java
+++ src/kr/co/garam/multiplex/MultiplexMain.java
@@ -6,20 +6,21 @@ public class MultiplexMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        // 구구단을 출력하는 테스트
-		
-		Scanner scanner = new Scanner(System.in);
+		// 구구단을 출력하는 테스트
+
+		var scanner = new Scanner(System.in);
 		String inputValue = scanner.nextLine();
-		
-		String[] splitValue = inputValue.split(",") ;
-		
-		int first = Integer.parseInt(splitValue[0]) ;
-		int second = Integer.parseInt(splitValue[1]) ;
-		
-	    for	(int i = first; i <= second; i++) {
-	    	int[] Result = Multiplex.Calc(i, second) ;
-	    	Multiplex.print(Result);
-	    }
+
+		String[] splitedValue = inputValue.split(",");
+
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+
+		for (int i = first; i <= second; i++) {
+			int[] Result = Multiplex.Calc(i, second);
+			Multiplex.print(Result);
+		}
+		scanner.close();
 	}
 
 }
